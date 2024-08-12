6ae23ef5b966fd492708ddb959d724737b7a6c99
litim smail <litimsmail@gmail.com>
2024-08-12 13:43:28 +0100

    FillGaps

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.module3.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-              .        ("Fixed");
+        System.out.println ("Fixed\n");
+
+
     }
 }
